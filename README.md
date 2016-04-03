b4961952163f21faf94e80396a0475627ee31593
Rafael T. Costella <rafael.costella@zrp.com.br>
2016-04-03 20:54:47 -0300

    Fixed gulptask and improved README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# gulp-task-transpiler-es6
-A simple transpiler from ES6 -> ES5 based on gulp
+# ES6 -> ES5 Transpiler (Gulp + CommonJS)
+
+## Requirements:
+  - nodejs: https://nodejs.org/en/download/package-manager/
+
+We will be using nodejs to run our package.json in order to get all the files needed to the transpiler.
+You can use this transpiler without running the installation proccess, however you will need to handle your dependencies.
+
+
+## Installation
+
+To install gulp and hist dependencies, simply run:
+
+```npm install```
+
+**OBS**: You can jump this section and install all files manually. This tutorial assume that all your dependencies will be saved inside the folder /node_modules.
+
+## Running gulp
+
+To start the process simply run:
+
+``` node node_modules/gulp/bin/gulp ```
+
+The task will be executed everytime that you save an es6 file automagically.
+Hit ```ctrl + c ``` to stop the process.
+
+## Configuration
+
+In order to modify the folder in which your files are saved just change the variable ```paths``` in line 7.
+
+## Additional References
+
+- [ZRP Blog Post]("http://labs.zrp.com.br/2016/03/28/transpilando-ecma-6-para-ecma-5/")
--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,15 @@ var gulp = require('gulp'),
     babel = require('gulp-babel'),
     sourcemaps = require('gulp-sourcemaps'),
     commonjsWrap = require('gulp-wrap-commonjs'),
-    concat = require('gulp-concat'),
-    jade = require('gulp-jade'),
-    karma = require('karma');
+    concat = require('gulp-concat');
 
 var paths = {
-    es6PathComponents: ['app/assets/javascripts/components/**/*.es6',
-                        'lib/**/assets/javascripts/components/**/**.es6'],
-    es6PathViews: 'app/assets/javascripts/views/**/*.es6',
-    templatePath: ['app/assets/javascripts/components/**/*.jade',
-                   'lib/**/**.jade'],
-    builds: 'app/assets/javascripts/builds',
+    es6Paths: ['src/js/**/*.es6'],
+    jsPaths: 'src/js/builds/build.js',
 };
 
-
-gulp.task('babel:views', function() {
-  return gulp.src([paths.es6PathViews])
-      .pipe(sourcemaps.init())
-      .pipe(babel({modules: 'common', moduleIds: true, moduleRoot: ''}))
-      .pipe(
-          commonjsWrap({
-            autoRequire: false,
-            pathModifier: function (path) {
-              path = path.replace(/.js$/, '')
-                      .replace(/^.*\/views/, 'views')
-
-              return path;
-            }
-          })
-      )
-      .pipe(concat('views.build.js'))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(paths.builds));
-});
-
 gulp.task('babel:components', function() {
-  return gulp.src(paths.es6PathComponents)
+  return gulp.src(paths.es6Paths)
       .pipe(sourcemaps.init())
       .pipe(babel({modules: 'common', moduleIds: true, moduleRoot: ''}))
       .pipe(
@@ -53,44 +26,11 @@ gulp.task('babel:components', function() {
       )
       .pipe(concat('components.build.js'))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest(paths.builds));
-});
-
-gulp.task('jade:templates', function() {
-  return gulp.src(paths.templatePath)
-      .pipe(sourcemaps.init())
-      .pipe(jade({
-        client: true,
-        pretty: true
-      }))
-      .pipe(
-          commonjsWrap({
-            autoRequire: false,
-            pathModifier: function (path) {
-              path = path.replace(/.js$/, '')
-                      .replace(/^.*\/components/, 'templates')
-
-              return path;
-            },
-            moduleExports: 'template()'
-          })
-      )
-      .pipe(concat('templates.build.js'))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(paths.builds));
-});
-
-gulp.task('spec', function (done) {
-  new karma.Server({
-    configFile: __dirname + '/karma.conf.js'
-  }, done).start();
+      .pipe(gulp.dest(paths.jsPaths));
 });
 
 gulp.task('js:compiler', function() {
-  gulp.watch([paths.es6PathComponents], ['babel:components']);
-  gulp.watch([paths.es6PathViews], ['babel:views']);
-  gulp.watch([paths.templatePath], ['jade:templates']);
+  gulp.watch([paths.es6Paths], ['babel:components']);
 });
 
-gulp.task('default', ['js:compiler', 'babel:components',
-                      'babel:views', 'jade:templates']);
+gulp.task('default', ['js:compiler', 'babel:components']);
